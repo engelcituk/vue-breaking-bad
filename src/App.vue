4b039f16d5994cdfde5ce98b52a0e1dddcdb6460
cituk <ecituk@thefives.com.mx>
2022-12-11 12:14:22 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  import {  ref } from 'vue';
+  const counter = ref<number>(0);
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Counter: {{counter}}</h1>
+    <button @click="counter++">+1</button>
+    <p>Mundo</p>
+  </div>
+</template>
+
+<style scoped>
+.wrapper{
+  display: flex;
+  flex-direction: column;
+}
+</style>
